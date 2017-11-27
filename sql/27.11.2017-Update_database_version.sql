2c5bb686477309aab04886d30eb5f68cb43d44de
Maksim <inc723@gmail.com>
2017-11-27 23:04:54 +0300

    Начало работы с базой.

---
+++ sql/27.11.2017-Update_database_version.sql
@@ -0,0 +1,4 @@
+-- Start Develop STDB434.01 
+-- By Inc (c) Stormscale.Ru.
+ 
+REPLACE INTO `version` VALUES ('Stormscale Cata Core 4.3.4 Fun Rev 1 used SDB434.01 rev. 27.11.2017 03:33:21 +4.3.4 (StormscaleDev) (Unix, Release)', 'StormscaleDev', 'STDB434.01', '1');
