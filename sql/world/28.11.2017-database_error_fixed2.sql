4bb9881b138a64e78f11d887ace580a8b7e9dc3f
Maksim <inc723@gmail.com>
2017-11-28 17:49:54 +0300

    Исправление ошибок

---
+++ sql/world/28.11.2017-database_error_fixed2.sql
@@ -0,0 +1,8 @@
+﻿-- world staring error fixed part1
+-- By Inc for Stormscale.Ru
+-- in World DataBase.
+
+
+INSERT INTO `creature_equip_template` (`entry`, `id`) VALUES ('35234', '1');
+
+UPDATE `creature` SET `equipment_id`='1' WHERE (`guid`='35234');
