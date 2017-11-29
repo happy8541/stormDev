9a7434080cb09ae4dd5c3226c0bf3d161d895599
Declipe <Declipe@yandex.ru>
2017-11-29 22:35:06 +0300

    Well_off_eternity Part2 fix

--- src/server/scripts/Kalimdor/CavernsOfTime/WellOfEternity/well_of_eternity.cpp
+++ src/server/scripts/Kalimdor/CavernsOfTime/WellOfEternity/well_of_eternity.cpp
@@ -937,7 +937,7 @@ public:
         case NPC_ABYSSAL:
         case NPC_VAROTHEN:
         case NPC_DOOMGUARD:
-            if (creature->Alive())
+            if (creature->IsAlive())
                 creature->Respawn(true);
             break;
         }
@@ -977,7 +977,7 @@ public:
             GetCreatureListWithEntryInGrid(creatures, me, me->GetEntry(), 500.0f);
             if (!creatures.empty())
                 for (std::list<Creature*>::const_iterator itr = creatures.begin(); itr != creatures.end(); ++itr)
-                    if ((*itr)->InCombat())
+                    if ((*itr)->IsInCombat())
                         (*itr)->AI()->AttackStart(who);
         }
 
