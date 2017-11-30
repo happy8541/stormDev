f7960bba68364889fae18dbad644d79e4020e993
Maksim <inc723@gmail.com>
2017-11-30 08:37:03 +0300

    goblin,worgen zones/quest

--- sql/world/30.11.2017-Update_database_version.sql
+++ sql/world/30.11.2017-Update_database_version.sql
@@ -3,4 +3,4 @@
 -- Все обновления базы включены с 27.11.17
 -- By Inc (c) Stormscale.Ru.
  
-REPLACE INTO `version` VALUES ('Stormscale Cata Core 4.3.4 Rev 2 29.11.2017 22:33:21 +4.3.4 (StormscaleDev) (Unix, Release)', 'StormscaleDev', 'STDB434.02', '1');
+REPLACE INTO `version` VALUES ('Stormscale Cata Core 4.3.4 Rev 2 29.11.2017 22:33:21 +4.3.4 (StormscaleDev) (Unix, Release)', 'StormscaleDev', 'STDB434.03', '1');
