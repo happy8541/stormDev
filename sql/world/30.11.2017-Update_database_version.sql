a7fc6872a8e50edc73347e5f123704ec08543df2
Maksim <inc723@gmail.com>
2017-11-30 08:36:17 +0300

    goblin,worgen zones/quest

---
+++ sql/world/30.11.2017-Update_database_version.sql
@@ -0,0 +1,6 @@
+﻿-- Start Develop STDB434.03 Ворген зона, гоблин зона, рабочая цепочка квестов. Корректная смена фаз в гилнеасе. 
+-- Скул апдейта не для паблика. 
+-- Все обновления базы включены с 27.11.17
+-- By Inc (c) Stormscale.Ru.
+ 
+REPLACE INTO `version` VALUES ('Stormscale Cata Core 4.3.4 Rev 2 29.11.2017 22:33:21 +4.3.4 (StormscaleDev) (Unix, Release)', 'StormscaleDev', 'STDB434.02', '1');
