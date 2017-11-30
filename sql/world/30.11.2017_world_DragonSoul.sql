aeb80eaa6b18e9fe49c7dfa2ea692b6caa927e39
Maksim <inc723@gmail.com>
2017-11-30 21:09:37 +0300

    Update DragonSoul

---
+++ sql/world/30.11.2017_world_DragonSoul.sql
@@ -0,0 +1,69 @@
+﻿-- Start Develop STDB434.01 
+-- By Inc (c) Stormscale.Ru.
+-- Released 30.11.2017
+-- Stormscale.Ru Developers - Inc;Declipe.
+
+
+
+UPDATE creature_template SET ScriptName = 'npc_eiendormi' WHERE entry = 57288;
+UPDATE creature_template SET ScriptName = 'npc_valeera_tele' WHERE entry = 57289;
+UPDATE creature_template SET ScriptName = 'npc_nethestrasz' WHERE entry = 57287;
+UPDATE creature_template SET ScriptName = 'travel_to_wyrmrest_base' WHERE entry = 57328;
+UPDATE creature_template SET ScriptName = 'travel_to_the_eye_of_eternity' WHERE entry = 57377;
+UPDATE creature_template SET ScriptName = 'travel_to_the_deck_of_the_skyfire' WHERE entry = 57378;
+
+UPDATE creature_template SET ScriptName = 'npc_tes' WHERE entry = 56848;
+UPDATE creature_template SET ScriptName = 'npc_ted' WHERE entry = 56854;
+UPDATE creature_template SET ScriptName = 'npc_goriona' WHERE entry = 56781;
+UPDATE creature_template SET InhabitType = '4' WHERE entry = 56781;
+UPDATE creature_template SET ScriptName = 'npc_tendones' WHERE entry = 56341;
+UPDATE creature_template SET unit_flags = '4' WHERE entry = 56341;
+UPDATE creature_template SET ScriptName = 'npc_tentaculo' WHERE entry = 53891;
+UPDATE creature_template SET unit_flags = '4' WHERE entry = 53891;
+
+UPDATE creature_template SET AIName = '' WHERE entry = 57288;
+UPDATE creature_template SET AIName = '' WHERE entry = 57289;
+UPDATE creature_template SET AIName = '' WHERE entry = 57287;
+UPDATE creature_template SET AIName = '' WHERE entry = 57328;
+UPDATE creature_template SET AIName = '' WHERE entry = 57377;
+UPDATE creature_template SET AIName = '' WHERE entry = 57378;
+
+UPDATE creature_template SET AIName = '' WHERE entry = 56848;
+UPDATE creature_template SET AIName = '' WHERE entry = 56854;
+UPDATE creature_template SET AIName = '' WHERE entry = 56781;
+UPDATE creature_template SET AIName = '' WHERE entry = 56341;
+UPDATE creature_template SET AIName = '' WHERE entry = 53891;
+
+UPDATE creature_template SET ScriptName = 'boss_deathwing' WHERE entry = 56173;
+UPDATE creature_template SET ScriptName = 'npc_maelstrom_trall' WHERE entry = 56103;
+UPDATE creature_template SET ScriptName = 'npc_arm_tentacle_one' WHERE entry = 56167;
+UPDATE creature_template SET ScriptName = 'npc_arm_tentacle_two' WHERE entry = 56846;
+UPDATE creature_template SET ScriptName = 'npc_wing_tentacle_one' WHERE entry = 56168;
+UPDATE creature_template SET ScriptName = 'npc_wing_tentacle_two' WHERE entry = 57972;
+UPDATE creature_template SET ScriptName = 'Teleportation_to_the_platforms' WHERE entry = 56307;
+
+UPDATE creature_template SET ScriptName = 'npc_ysera_dw' WHERE entry = 56100;
+UPDATE creature_template SET ScriptName = 'npc_alextstraza_dw' WHERE entry = 56099;
+UPDATE creature_template SET ScriptName = 'npc_nozdormu_dw' WHERE entry = 56102;
+UPDATE creature_template SET ScriptName = 'npc_kalecgos_dw' WHERE entry = 56101;
+
+UPDATE creature_template SET unit_flags = '4' WHERE entry = 56173;
+UPDATE creature_template SET unit_flags = '4' WHERE entry = 57694;
+UPDATE creature_template SET unit_flags = '4' WHERE entry = 57686;
+UPDATE creature_template SET unit_flags = '4' WHERE entry = 57696;
+UPDATE creature_template SET unit_flags = '4' WHERE entry = 57695;
+UPDATE creature_template SET unit_flags = '4' WHERE entry = 56844;
+UPDATE creature_template SET unit_flags = '4' WHERE entry = 56846;
+UPDATE creature_template SET unit_flags = '4' WHERE entry = 56167;
+UPDATE creature_template SET unit_flags = '4' WHERE entry = 57972;
+UPDATE creature_template SET unit_flags = '4' WHERE entry = 56168;
+
+
+UPDATE creature_template SET InhabitType = '4' WHERE entry = 56173;
+UPDATE creature_template SET InhabitType = '4' WHERE entry = 57694;
+UPDATE creature_template SET InhabitType = '4' WHERE entry = 57686;
+UPDATE creature_template SET InhabitType = '4' WHERE entry = 57696;
+UPDATE creature_template SET InhabitType = '4' WHERE entry = 57695;
+UPDATE creature_template SET InhabitType = '4' WHERE entry = 56844;
+UPDATE creature_template SET InhabitType = '4' WHERE entry = 56846;
+UPDATE creature_template SET InhabitType = '4' WHERE entry = 56167;
