376e5bcd58244dad5d5ca6cb0d7fb66d56141113
Maksim <inc723@gmail.com>
2017-12-02 21:31:42 +0300

    SQL fix

---
+++ sql/world/Fix_errors_stdb03.sql
@@ -0,0 +1,18 @@
+﻿/* Update and Fixed STDB03
+Only for Stormscale.Ru Cataclysm WoW-Emulator.
+In STDBWORLD.
+
+by Inc
+*/
+
+/*
+апдейт нпс 42419;42454 отсутсвите АИ.
+*/
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE (`entry`='42419')
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE (`entry`='42452')
+
+/*
+Отсутсвие String
+ВНИМАНИЕ: Ренейм строчки, или-же ее отсутсвие приведет к крашу клиента!
+*/
+INSERT INTO `arkcore_string` (`entry`, `content_default`) VALUES ('29245', 'Stormscale.Ru Cataclysm WoW-Emulator');
