f7de096d22c768b58e51855798e6fc46d5502544
Maksim <inc723@gmail.com>
2017-12-09 13:46:00 +0300

    rename sec names; fix in database levelsecurity

--- src/server/scripts/Custom/gm_hello.cpp
+++ src/server/scripts/Custom/gm_hello.cpp
@@ -12,22 +12,22 @@
         
        if (player->GetSession()->GetSecurity() == SEC_MODERATOR)  
               {  
-                  std::string sText = ("Модератор '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' входит в Игровой мир.");  
+                  std::string sText = ("Эвент-Мастер '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' входит в Игровой мир.");  
                   sWorld->SendServerMessage(SERVER_MSG_STRING, sText.c_str());  
               }  
 if (player->GetSession()->GetSecurity() == SEC_TESTER)  
               {  
-                  std::string sText = ("Гейм-Мастер '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' входит в Игровой мир.");  
+                  std::string sText = ("Модератор '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' входит в Игровой мир.");  
                   sWorld->SendServerMessage(SERVER_MSG_STRING, sText.c_str());  
               }
     if (player->GetSession()->GetSecurity() == SEC_GAMEMASTER)  
               {  
-                  std::string sText = ("Администратор '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' входит в Игровой мир.");  
+                  std::string sText = ("Гейм-Мастер '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' входит в Игровой мир.");  
                   sWorld->SendServerMessage(SERVER_MSG_STRING, sText.c_str());  
               }  
     if (player->GetSession()->GetSecurity() == SEC_ADMINISTRATOR)  
               {  
-                  std::string sText = ("VIP1 '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' входит в Игровой мир.");  
+                  std::string sText = ("Администратор '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' входит в Игровой мир.");  
                   sWorld->SendServerMessage(SERVER_MSG_STRING, sText.c_str());  
               } 
 	if (player->GetSession()->GetSecurity() == SEC_CONSOLE)
@@ -46,23 +46,23 @@ if (player->GetSession()->GetSecurity() == SEC_TESTER)
   {   
        if (player->GetSession()->GetSecurity() == SEC_MODERATOR)  
               {  
-                  std::string sText = ("Модератор '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' выходит из Игрового мира.");  
+                  std::string sText = ("Эвент-Мастер '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' выходит из Игрового мира.");  
                   sWorld->SendServerMessage(SERVER_MSG_STRING, sText.c_str());  
               }  
 
 if (player->GetSession()->GetSecurity() == SEC_TESTER)  
               {  
-                  std::string sText = ("Гейм-Мастер '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' выходит из Игрового мира.");  
+                  std::string sText = ("Модератор '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' выходит из Игрового мира.");  
                   sWorld->SendServerMessage(SERVER_MSG_STRING, sText.c_str());  
               } 
     if (player->GetSession()->GetSecurity() == SEC_GAMEMASTER)  
               {  
-                  std::string sText = ("Администратор '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' выходит из Игрового мира.");  
+                  std::string sText = ("Гейм-Мастер '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' выходит из Игрового мира.");  
                   sWorld->SendServerMessage(SERVER_MSG_STRING, sText.c_str());  
               }  
     if (player->GetSession()->GetSecurity() == SEC_ADMINISTRATOR)  
               {  
-                  std::string sText = ("VIP1 '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' выходит из Игрового мира.");  
+                  std::string sText = ("Администратор '|cFF60FF00" + std::string(player->GetSession()->GetPlayerName()) + "|r' выходит из Игрового мира.");  
                   sWorld->SendServerMessage(SERVER_MSG_STRING, sText.c_str());  
               } 
 	if (player->GetSession()->GetSecurity() == SEC_CONSOLE)
