5b165f3835762b861daee1df812efa3460b17aa4
Maksim <inc723@gmail.com>
2017-12-30 16:22:57 +0300

    smart

---
+++ sql/world/30.12.17_world_update.sql
@@ -0,0 +1,6 @@
+﻿-- Start Develop STDB434.04 add missing smart 
+-- Скул апдейта не для паблика. 
+-- Все обновления базы включены с 27.11.17
+-- By Inc (c) Stormscale.Ru.
+ 
+REPLACE INTO `version` VALUES ('Stormscale Cata Core 4.3.4 Rev 4 30.12.2017 16:21:01 +4.3.4 (StormscaleDev) (Unix, Release)', 'StormscaleDev', 'STDB434.04 & "patch" SmartAI Lab 434.12', '1');
